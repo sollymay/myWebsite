061668dcd7dbe206e182d8fc9a98cea0db7671e5
sollymay <sollymay@gmail.com>
2016-05-04 17:03:23 -0400

    added wikiSearch submodule

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
+$(document).ready(function (){
+  $(".home").click(function (){
+    $('html, body').animate({
+      scrollTop: $(".home-target").offset().top -65
+    }, 1000);
+  });
 
+  $(".portfolio").click(function (){
+    $('html, body').animate({
+      scrollTop: $(".portfolio-target").offset().top -100
+    }, 2000);
+  });
+
+  $(".contact").click(function (){
+    $('html, body').animate({
+      scrollTop: $(".contact-target").offset().top -100
+    }, 2000);
+  });
+
+});
