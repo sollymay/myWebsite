13e498a34271815ff093ead6db13040113db96d2
sollymay <sollymay@gmail.com>
2016-07-01 13:29:03 -0400

    fixed scrolling animation using scrollTop

--- js/main.js
+++ js/main.js
@@ -1,20 +1,3 @@
 $(document).ready(function (){
-  $(".home").click(function (){
-    $('html, body').animate({
-      scrollTop: $(".home-target").offset().top -65
-    }, 1000);
-  });
-
-  $(".portfolio").click(function (){
-    $('html, body').animate({
-      scrollTop: $(".portfolio-target").offset().top -100
-    }, 2000);
-  });
-
-  $(".contact").click(function (){
-    $('html, body').animate({
-      scrollTop: $(".contact-target").offset().top -100
-    }, 2000);
-  });
-
+ $('.nav').localScroll({offset: {top:-110}});
 });
