77d8a800b60f79949c5336e1bf6ffcf25d4e92a0
Salomon May <sollymay@gmail.com>
2016-08-16 19:31:34 -0400

    added initial screen js handling

--- sub2/numbersGame/js/main.js
+++ sub2/numbersGame/js/main.js
@@ -1,3 +1,14 @@
 $(document).ready(function (){
   $(".player").mb_YTPlayer();
+
+  $( "#startButton" ).click(function() {
+
+    $( "#content" ).fadeOut( "slow", function() {
+      // Animation complete.
+      
+    });
+
+
+  });
+
 });
