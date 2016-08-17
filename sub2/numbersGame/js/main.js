cf09c40767ddf72be14fd70ec85f425354ceb5af
Salomon May <sollymay@gmail.com>
2016-08-17 00:43:44 -0400

    trying to fix the issue

--- sub2/numbersGame/js/main.js
+++ sub2/numbersGame/js/main.js
@@ -6,7 +6,7 @@ $(document).ready(function (){
     $( "#content" ).fadeOut( "slow", function() {
       // Animation complete.
       $("#first-question").show()
-      while ($("#answer").val()!==str(1452)) {
+      while (str($("#answer").val())!==str(1452)) {
         console.log($("#answer").val())
 
       }
