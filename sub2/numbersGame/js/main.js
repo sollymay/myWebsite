38a83f2c9c2ea7a5cf0429bd83f09337954a58fd
Salomon May <sollymay@gmail.com>
2016-08-17 14:26:38 -0400

    added final functionality

--- sub2/numbersGame/js/main.js
+++ sub2/numbersGame/js/main.js
@@ -1,15 +1,38 @@
 $(document).ready(function (){
   $(".player").mb_YTPlayer();
   $("#first-question").hide();
+  $("#second-question").hide();
+  $("#third-question").hide();
+  $("#final-question").hide();
   $( "#startButton" ).click(function() {
-
-    $( "#content" ).fadeOut( "slow", function() {
-      // Animation complete.
-
-      $("#first-question").show()
-    });
-
+    fadeOutandIn("#content","#first-question")
+    handleAnswer("#first-question","#second-question",8,"#first-answer")
+    handleAnswer("#second-question","#third-question",18,"#second-answer")
+    handleAnswer("#third-question","#final-question",16,"#third-answer")
+    
 
   });
+  var fadeOutandIn = function(previous, current){
+    $(previous).fadeOut("slow", function() {
+      //Stuff to do *after* the animation takes place
+      $(current).show()
+    })
+  }
+  var handleAnswer = function(current, next, answer, fieldID){
+    $( fieldID)
+    .keypress(function(e) {
+      $(fieldID).css("color","#236692");
+      if(e.which ==13){
+        var value = $( this ).val();
+        if (value == answer){
+          fadeOutandIn(current,next)
 
+        }
+        else{
+          $(fieldID).css("color","red");
+
+        }
+      }
+    })
+  }
 });
