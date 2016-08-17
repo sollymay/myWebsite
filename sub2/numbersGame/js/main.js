67d740f1b156c2db3b6ce48ea26fd06831703fd7
Salomon May <sollymay@gmail.com>
2016-08-17 00:37:39 -0400

    checking first question

--- sub2/numbersGame/js/main.js
+++ sub2/numbersGame/js/main.js
@@ -1,11 +1,15 @@
 $(document).ready(function (){
   $(".player").mb_YTPlayer();
-
+  $("#first-question").hide();
   $( "#startButton" ).click(function() {
 
     $( "#content" ).fadeOut( "slow", function() {
       // Animation complete.
-      
+      $("#first-question").show()
+        console.log($("#answer").val())
+      if($("#answer").val()!==""){
+        alert("oy vey")
+      }
     });
 
 
