2a3927e93f953c5a7d36d00984cd520a4c8d5737
Salomon May <sollymay@gmail.com>
2016-08-17 00:47:06 -0400

    fixed question layout issue

--- sub2/numbersGame/js/main.js
+++ sub2/numbersGame/js/main.js
@@ -5,16 +5,8 @@ $(document).ready(function (){
 
     $( "#content" ).fadeOut( "slow", function() {
       // Animation complete.
+
       $("#first-question").show()
-      while (str($("#answer").val())!==str(1452)) {
-        console.log($("#answer").val())
-
-      }
-      else {
-        console.log("yesssss")
-
-      }
-
     });
 
 
